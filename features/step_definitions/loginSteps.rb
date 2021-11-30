7015aa856dd7d331594ce611b84e4e0c963d8d98
Mercedes <mercedes.htcv@gmail.com>
2021-11-30 15:16:28 -0400

    complete (five) screnarios successfully of login

--- features/step_definitions/loginSteps.rb
+++ features/step_definitions/loginSteps.rb
@@ -1,22 +1,44 @@
 Given('I have browsed to the Rottweiler’s Academy login page') do
-    page.driver.browser.manage.window.maximize
-     visit('https://www.r-acad.com/wp-login.php')
-  end
+  page.driver.browser.manage.window.maximize
+   visit('https://www.r-acad.com/wp-login.php')
+end
+
+Given('I have entered {string} into the email field') do |eMail|
+  fill_in 'log', :with => eMail
   
-  Given('I have entered {string} into the email field') do |eMail|
-    fill_in 'log', :with => eMail
-    
-  end
+end
+
+Given('I have entered {string} into the password field') do |password|
+  fill_in 'pwd', :with => password
+end
+
+When('I press the access button') do
+  xpath = '//*[@id="wp-submit"]'
+  find(:xpath, xpath).click
+end
+
+Then('I should see my welcome message {string} in the main page') do |userName|
+  page.should have_content(userName)
+end
+
+
+Then('I should see an error message {string}') do |errorMessage|
+  page.has_content?(errorMessage)
   
-  Given('I have entered {string} into the password field') do |password|
-    fill_in 'pwd', :with => password
-  end
-  
-  When('I press the access button') do
-    xpath = '//*[@id="wp-submit"]'
-    find(:xpath, xpath).click
-  end
-  
-  Then('I should see my welcome message {string} in the main page') do |userName|
-    page.should have_content(userName)
-  end
+end
+
+Then('I should see an message {string}') do |passwordMessage|
+  page.has_content?(passwordMessage)
+end
+
+Then('I should see {string} message') do |passwordEmpty|
+  page.has_content?(passwordEmpty)
+end
+
+Then('I should see {string} error message') do |noPassword|
+  page.has_content?(noPassword)
+end
+
+Given('I have entered {string} into de password field') do |justPwd|
+  fill_in 'pwd', :with => justPwd
+end
