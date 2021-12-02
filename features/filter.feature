9a57cd4c023fa6c3efa9b59fdc0aeae899797ff2
Daniel Acero <edanielacero@gmail.com>
2021-12-02 00:48:57 -0400

    filter scenario 1

--- features/filter.feature
+++ features/filter.feature
@@ -4,10 +4,15 @@ Feature: Filter
     I want to filter by order, category or instructor.
 
 Background:
-    Given I have browsed to the Rottweiler’s Academy course page
-    And I see the Rottweiler’s Academy full course catalog with "Cursos" title
+    Given I have browsed to the Rottweiler’s Academy login page
+    And I have entered "karol.zerna@ucb.edu.bo" into the email field
+    And I have entered "test_2021" into the password field
+    When I press the access button
+    Then I should see my welcome message "Hola Karol" in the main page
 
 Scenario: Filter by progress
-    Given I click on the button that says “Alfabetico”
+    Given I click on the Course section that is on the upper left corner of the page
+    And I see the Rottweiler’s Academy full course catalog with "Cursos" title
+    And I click on the button that says “Alfabetico”
     When I click on the “Mi progreso” text from the dropdown menu
-    Then I should see all courses I have signed up
+    Then I should see all courses I have signed up with the text "EN PROGRESO"
--- features/step_definitions/filterSteps.rb
+++ features/step_definitions/filterSteps.rb
@@ -3,18 +3,25 @@ Given('I have browsed to the Rottweiler’s Academy course page') do
     visit('https://www.r-acad.com/courses/')
   end
   
+Given('I click on the Course section that is on the upper left corner of the page') do
+    xpath = '/html/body/div[1]/header/div[1]/nav/div/ul/li[2]/a'
+    find(:xpath, xpath).click
+ end
+
   And('I see the Rottweiler’s Academy full course catalog with {string} title') do |title|
     page.should have_content(title)
   end
   
-  Given('I click on the button that says “Alfabetico”') do
-    pending # Write code here that turns the phrase above into concrete actions
+  And('I click on the button that says “Alfabetico”') do
+    xpath = '/html/body/div[1]/div/div/div/div/main/div/form/div[2]/div/div[1]/div[1]/select'
+    find(:xpath, xpath).click
   end
   
   When('I click on the “Mi progreso” text from the dropdown menu') do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+    xpath = '/html/body/div[1]/div/div/div/div/main/div/form/div[2]/div/div[1]/div[1]/select/option[3]'
+    find(:xpath, xpath).click
+ end
   
-  Then('I should see all courses I have signed up') do
-    pending # Write code here that turns the phrase above into concrete actions
+  Then('I should see all courses I have signed up with the text {string}') do |lastActivity|
+    page.should have_content(lastActivity)
   end
