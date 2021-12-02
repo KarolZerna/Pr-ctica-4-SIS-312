5300d38561c54eabf62586cefeeb018ccf5bd67a
Mercedes <mercedes.htcv@gmail.com>
2021-12-01 23:56:12 -0400

    events fixed

--- features/events.feature
+++ features/events.feature
@@ -8,8 +8,8 @@ Background:
     Then I see the Rottweiler’s Academy full course catalog with "Cursos" title
 
 Scenario: Check events
-    Given I click on the “Eventos” section that is on the upper left corner of the page
-    And I see the “Próximos Eventos” page
-    And I click on “Eventos Anteriores”
-    Then I should see a new page with Rottweiler’s Academy full list of events
+    Given I click on the Eventos section that is on the upper left corner of the page
+    When I see the "Próximos Eventos" page
+    And I click on Eventos Anteriores
+    Then I should see a new page about past events of Rottweiler’s Academy like "CURSO ONLINE: PRESUPUESTO EMPRESARIAL"
 
--- features/step_definitions/eventSteps.rb
+++ features/step_definitions/eventSteps.rb
@@ -1,13 +1,17 @@
-Given('I click on the 'Eventos' section that is on the upper left corner of the page') do
-    xpath = '//*[@id="wp-submit"]'
+Given('I click on the Eventos section that is on the upper left corner of the page') do
+    xpath = '/html/body/div[1]/header/div[1]/nav/div/ul/li[3]/a'
     find(:xpath, xpath).click
 end
   
-Given('I see the {string} page') do |page|
+When('I see the {string} page') do |page|
     page.should have_content(page)
 end
   
-Given('I click on “Eventos Anteriores”') do
-    xpath = '//*[@id="wp-submit"]'
+And('I click on Eventos Anteriores') do
+    xpath = '/html/body/div[1]/div/div/div/div/main/article/div/div/div[2]/div[3]/div[3]/nav/ul/li/a'
     find(:xpath, xpath).click
 end
+
+Then('I should see a new page about past events of Rottweiler’s Academy like {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
