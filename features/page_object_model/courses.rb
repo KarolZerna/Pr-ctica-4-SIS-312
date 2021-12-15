65e5e2252bafe93d42007be935b59d717c2148a7
KarolZerna <karol.zerna@gmail.com>
2021-12-14 21:33:56 -0400

    sign up

--- features/page_object_model/courses.rb
+++ features/page_object_model/courses.rb
@@ -5,10 +5,23 @@ class HomePage
 	include PageObject
 	page_url("https://www.r-acad.com/courses/")
 
-	#define elements in HOME PAGE
 	h4(:home_title, :xpath => '/html/body/div[1]/div/div/div/div/main/div/form/div[1]/h4')
 	link(:sign_up_link, :href => 'https://www.r-acad.com/registro/')
 	link(:log_in_link, :href => 'https://www.r-acad.com/wp-login.php')
-	#define functions used in HOME PAGE
+
 	
+end
+
+class SignUpPage
+	include PageObject
+	page_url("https://www.r-acad.com/registro/")
+
+	h1(:sign_up_title, :xpath => '/html/body/h1')
+	text_field(:mail, id: 'signup_email')
+	text_field(:password, id: 'signup_password')
+	text_field(:confirm_password, :id => 'signup_password_confirm')
+	text_field(:name, :id => 'field_1')
+	text_field(:lastname, :id => 'field_2')
+	text_field(:userName, :id => 'field_3')
+	button(:signup_submit, :value => 'Crear una cuenta')
 end
--- features/page_object_model/rspec_test.rb
+++ features/page_object_model/rspec_test.rb
@@ -25,5 +25,15 @@ describe "Test Home Page" do
 		@wait.until {home_page.home_title}
 		expect(home_page.home_title == "Cursos").to be true
 	end
+
+    it "loads sign up page" do
+		home_page = HomePage.new(@driver)
+		home_page.goto
+		@wait.until {home_page.sign_up_link_element}
+		home_page.sign_up_link
+		sign_up_page = SignUpPage.new(@driver)
+		@wait.until {sign_up_page.sign_up_title}
+		expect(sign_up_page.sign_up_title == "Crear una cuenta").to be true
+	end
 end
 
